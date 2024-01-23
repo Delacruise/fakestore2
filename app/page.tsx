048b2594c73839a61c3cc6c012935d3fb4483565
Luke Daffue <25479590+Delacruise@users.noreply.github.com>
2024-01-23 10:49:29 +0400

    WIP(Prep): Public images and home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main>
-    <div>Main page</div>
-  </main>;
+  return (
+    <main>
+      <div>Sliding Banner</div>
+      <div>Page filler</div>
+      <div>Call to Action</div>
+      <div>Call to action Banner</div>
+      <div>Testimonials</div>
+    </main>
+  );
 }
