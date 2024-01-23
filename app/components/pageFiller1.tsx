587423f1a52e2a25fee0f297bf7464663457676c
Luke Daffue <25479590+Delacruise@users.noreply.github.com>
2024-01-23 14:40:46 +0400

    WIP(PageFiller): Implement page filler info-graphs

--- app/components/pageFiller1.tsx
+++ app/components/pageFiller1.tsx
@@ -1,5 +1,69 @@
 import React from 'react';
 
 export default function pageFiller1() {
-  return <div>pageFiller1</div>;
+  return (
+    <>
+      <div className='w-full p-4 flex justify-around containerSpacing'>
+        <div className='p-4 flex justify-items-center items-center text-gray-400'>
+          <div className='pr-4 text-orange-600'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='w-6 h-6'
+            >
+              <path d='M3.375 4.5C2.339 4.5 1.5 5.34 1.5 6.375V13.5h12V6.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM13.5 15h-12v2.625c0 1.035.84 1.875 1.875 1.875h.375a3 3 0 116 0h3a.75.75 0 00.75-.75V15z' />
+              <path d='M8.25 19.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0zM15.75 6.75a.75.75 0 00-.75.75v11.25c0 .087.015.17.042.248a3 3 0 015.958.464c.853-.175 1.522-.935 1.464-1.883a18.659 18.659 0 00-3.732-10.104 1.837 1.837 0 00-1.47-.725H15.75z' />
+              <path d='M19.5 19.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z' />
+            </svg>
+          </div>
+          <div>
+            <div className='text-gray-600 font-bold text-2xl'>Free Shipping</div>
+            <div className='text-xl'>Free shipping on orders</div>
+          </div>
+        </div>
+        <div className='p-4 flex justify-items-center items-center text-gray-400'>
+          <div className='pr-4 text-orange-600'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='w-6 h-6'
+            >
+              <path d='M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z' />
+              <path
+                fillRule='evenodd'
+                d='M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z'
+                clipRule='evenodd'
+              />
+            </svg>
+          </div>
+          <div>
+            <div className='text-gray-600 font-bold text-2xl'>Returns</div>
+            <div className='text-xl'>30 Days return policy</div>
+          </div>
+        </div>
+        <div className='p-4 flex justify-items-center items-center text-gray-400'>
+          <div className='pr-4 text-orange-600'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='w-6 h-6'
+            >
+              <path
+                fillRule='evenodd'
+                d='M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-4.28 9.22a.75.75 0 000 1.06l3 3a.75.75 0 101.06-1.06l-1.72-1.72h5.69a.75.75 0 000-1.5h-5.69l1.72-1.72a.75.75 0 00-1.06-1.06l-3 3z'
+                clipRule='evenodd'
+              />
+            </svg>
+          </div>
+          <div>
+            <div className='text-gray-600 font-bold text-2xl'>100% Money Back</div>
+            <div className='text-xl'>If the item didn't suit you</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import SLIDINGBANNERS from './components/slidingBanner';
+import PAGEFILLER1 from './components/pageFiller1'
 
 export default function Home() {
   return (
     <main className='p-4 h-full min-h-fit container-xl'>
       <SLIDINGBANNERS />
-      <div>Page filler</div>
+      <PAGEFILLER1 />
       <div>Call to Action</div>
       <div>Call to action Banner</div>
       <div>Testimonials</div>
